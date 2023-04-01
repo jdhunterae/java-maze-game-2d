9b3453aaf727090ee7e8f8ee48104e708e3e426e
Andrew Pomerleau <arpomerleau@gmail.com>
2023-04-01 17:58:51 -0400

    Create JFrame window.

---
+++ src/main/java/maze/game/Main.java
@@ -0,0 +1,15 @@
+package maze.game;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Maze Game 2d - Ep 1");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
