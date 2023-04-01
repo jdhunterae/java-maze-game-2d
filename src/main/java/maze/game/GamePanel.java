e3d73655d7e4916fc0cd8493afc8a9f3c575e3cc
Andrew Pomerleau <arpomerleau@gmail.com>
2023-04-01 18:38:52 -0400

    Add thread to game.

--- src/main/java/maze/game/GamePanel.java
+++ src/main/java/maze/game/GamePanel.java
@@ -3,7 +3,7 @@ package maze.game;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3;
 
@@ -13,9 +13,20 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
     }
 }
